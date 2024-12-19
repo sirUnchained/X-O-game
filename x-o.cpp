7763b48ad05cffc5db0a2bce021497e15b60fc17
sirUnchained <bbehnam3831@gmail.com>
2024-12-19 11:47:43 +0330

    added most of needed functions.

--- x-o.cpp
+++ x-o.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <array>
 
+char check_winner(const std::array<std::array<char, 3>, 3> &board);
+bool fill_house(char side, std::array<std::array<char, 3>, 3> &board);
+void show_board(const std::array<std::array<char, 3>, 3> &board, int round);
+
 int main(int argc, char *argv[])
 {
   std::array<std::array<char, 3>, 3> board{{
@@ -30,11 +34,87 @@ int main(int argc, char *argv[])
 
   std::cout << "\nYou are \"" << user_side << "\".\nI am \"" << pc_side << "\".\nNOW LETS PLAY !\n\n";
 
-  for (int i = 1; check_winner(board) != 'n'; i++)
+  for (int i = 1; check_winner(board) == 'c'; i++)
   {
     show_board(board, i);
     fill_house(user_side, board);
   }
 
   std::cout << check_winner(board);
+}
+
+char check_winner(const std::array<std::array<char, 3>, 3> &board)
+{
+  for (int i = 0; i < size(board); i++)
+  {
+    for (int j = 0; j < size(board); j++)
+    {
+      if (board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j])
+      {
+        return board[i][j];
+      }
+      else if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2])
+      {
+        return board[i][j];
+      }
+      else if (board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2])
+      {
+        return board[i][j];
+      }
+      else if (board[i][j + 2] == board[i + 1][j + 1] && board[i][j + 2] == board[i][j])
+      {
+        return board[i][j];
+      }
+    }
+  }
+  return 'c';
+}
+
+bool fill_house(char side, std::array<std::array<char, 3>, 3> &board)
+{
+  int house{0};
+  std::cout << "Which house you wanna fill : ";
+  std::cin >> house;
+  if (house > 9 || house < 0)
+  {
+    std::cout << "Out of Houses ! enter a valid one.\n";
+    return false;
+  }
+
+  int count{0};
+  for (int i = 0; i < size(board); i++)
+  {
+    for (int j = 0; j < size(board); j++)
+    {
+      count++;
+      if (count == house)
+      {
+        if (board[i][j] == 'X' || board[i][j] == 'O')
+        {
+          std::cout << "\nThe house is already filled with \"" << board[i][j] << "\".\n";
+          return false;
+        }
+        board[i][j] = side;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void show_board(const std::array<std::array<char, 3>, 3> &board, int round)
+{
+  std::cout << "Round \"" << round << "\" :\n";
+  for (int i = 0; i < size(board); i++)
+  {
+    for (int j = 0; j < size(board); j++)
+    {
+      std::cout << board[i][j];
+      if (j != 2)
+        std::cout << '|';
+    }
+    if (i != 2)
+      std::cout << "\n_ _ _\n";
+  }
+  std::cout << "\n\n";
 }
