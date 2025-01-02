56b8292479ddf32006093da2f29b50dd00621c7a
sirUnchained <bbehnam3831@gmail.com>
2025-01-02 13:21:31 +0330

    format

--- index.cpp
+++ index.cpp
@@ -99,12 +99,12 @@ int pc_chose_house()
 
 char check_winner()
 {
- for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
   {
-      if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
-	return board[i][0];
-      if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
-	return board[0][i];
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+      return board[i][0];
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+      return board[0][i];
   }
 
   if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
@@ -115,14 +115,14 @@ char check_winner()
   {
     return board[0][2];
   }
-    
+
   for (const auto &row : board)
   {
-     for (const auto &home : row)
-	{
-	    if (home != 'X' && home != 'O')
-		return 'c';
-	}
+    for (const auto &home : row)
+    {
+      if (home != 'X' && home != 'O')
+        return 'c';
+    }
   }
 
   return '=';
